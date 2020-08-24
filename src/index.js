874c3f57a64e440b336065a5a40806fc3da79c59
Jesse Halvorsen <atelaes@gmail.com>
2020-08-23 23:23:34 -0500

    Started working on file operations.  Working on understanding the setup of DVD rip files so as to accurately detect them.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const electron = require('electron');
 const { ipcMain, dialog } = require('electron')
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const ReadWrite = require("./ReadWrite.js");
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const cp = require('child_process');
-const ReadWrite = require("./ReadWrite.js");
+const videoFileExtentions = ['mp4', 'mkv']
+
 
 app.whenReady().then(createWindow);
 
@@ -16,12 +21,48 @@ function createWindow() {
     }
   })
   //let userData = new ReadWrite({configName: 'userData', defaults: {}});
-  console.log(app.getPath('userData'));  //userData.set('working', 'Hell yeah!');
+  //console.log(app.getPath('userData'));  //userData.set('working', 'Hell yeah!');
   win.webContents.openDevTools()
   //var child = cp.spawn('ffplay', ['E:\\DVD Movies\\Moana.mp4']);
 
   win.loadFile('src/index.html')
 
+}
+
+function findVideosFromFile(file) {
+  fs.readdir(file, {withFileTypes : true}, function (err, subfiles) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    /* files.forEach(function (file, default) {
+        // Do whatever you want to do with the file
+        console.log(file);
+    });*/
+    for (let i=0; i<subfiles.length; i++) {
+      let subfile = subfiles[i];
+      if (subfile.isDirectory()) {
+        //console.log('Going through a DVD folder');
+        let subAddress = path.join(file, subfile.name);
+        const getDirectories = fs.readdirSync(subAddress, { withFileTypes: true })
+        getDirectories.map(subsubItem => {
+            if (subsubItem.isDirectory()) {
+              //console.log('Going through a DVD subfolder');
+              if (!['VIDEO_TS', 'AUDIO_TS', 'JACKET_P', 'common', 'win'].includes(subsubItem.name)) {
+                console.log('Found a non-audio/video folder: ' + subsubItem.name + ' in ' + subfile.name);
+              } else {
+                //console.log('exp');
+              }
+            }
+          })
+      }
+    }
+  });
+}
+
+function isDVDRip(folder) {
+  
 }
 
 ipcMain.on('settings-watchfolder-select', (event) => {
@@ -33,5 +74,6 @@ ipcMain.on('settings-watchfolder-select', (event) => {
 })})
 
 ipcMain.on('settings-watchfolder-add', (event, arg) => {
-  console.log(arg);
+  //Add to library
+  findVideosFromFile(arg['address'], arg['type']);
 })
