c47a2b501f767331e86bc8cfad94ca23cc3cb692
Jesse Halvorsen <atelaes@gmail.com>
2020-08-24 14:28:24 -0500

    Basic file searching and DVD detection now working.  Just adds items to empty library object for now, does not push to render.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ const path = require('path');
 const ReadWrite = require("./ReadWrite.js");
 
 const app = electron.app;
+let library = {"settings" : {}, "playlists" : [], "collections" : [], "media" : []};
 const BrowserWindow = electron.BrowserWindow;
-const videoFileExtentions = ['mp4', 'mkv']
-
 
 app.whenReady().then(createWindow);
 
@@ -29,40 +28,96 @@ function createWindow() {
 
 }
 
-function findVideosFromFile(file) {
-  fs.readdir(file, {withFileTypes : true}, function (err, subfiles) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    /* files.forEach(function (file, default) {
-        // Do whatever you want to do with the file
-        console.log(file);
-    });*/
-    for (let i=0; i<subfiles.length; i++) {
-      let subfile = subfiles[i];
-      if (subfile.isDirectory()) {
-        //console.log('Going through a DVD folder');
-        let subAddress = path.join(file, subfile.name);
-        const getDirectories = fs.readdirSync(subAddress, { withFileTypes: true })
-        getDirectories.map(subsubItem => {
-            if (subsubItem.isDirectory()) {
-              //console.log('Going through a DVD subfolder');
-              if (!['VIDEO_TS', 'AUDIO_TS', 'JACKET_P', 'common', 'win'].includes(subsubItem.name)) {
-                console.log('Found a non-audio/video folder: ' + subsubItem.name + ' in ' + subfile.name);
-              } else {
-                //console.log('exp');
-              }
-            }
-          })
+//Takes a full folder address and looks for videos in it,
+//adding any it finds to the library
+function findVideosFromFolder(folder) {
+  const videoExtensions = ['mp4', 'mkv', 'avi', 'webm', 'mov', 'wmv', 'flv', 'avchd']
+  if isDVDRip(folder) {
+    addDVDRip(folder)
+  } else {
+    fs.readdir(folder, {withFileTypes : true}, function (err, components) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
       }
-    }
-  });
+      for (let i=0; i<components.length; i++) {
+        let component = components[i];
+        let compAddress = path.join(folder, component.name);
+        if (component.isDirectory()) {
+          //console.log('Going through a DVD folder');
+          findVideosFromFolder(compAddress);
+        } else {
+          let fileExt = path.extname(component.name).replace('.', '').toLowerCase();
+          if (videoExtensions.includes(fileExt)) {
+            addVideoFile(compAddress);
+          }
+        }
+      }
+    });
+  }
 }
 
+//Takes a complete file address of a directory.
+// Returns a boolen on whether it thinks this is a DVD rip folder.
 function isDVDRip(folder) {
-  
+  let positiveEvidence = false;
+  let contents = fs.readdirSync(folder, { withFileTypes: true })
+  if (contents.length > 30) {
+    return false;
+  }
+  for (let i=0; i<contents.length; i++) {
+    let content = contents[i];
+    if (content.isDirectory()) {
+      if (content.name === 'VIDEO_TS') {
+        positiveEvidence = true;
+      } else if (!['VIDEO_TS', 'AUDIO_TS', 'JACKET_P', 'common', 'win'].includes(content.name)) {
+        return false;
+      }
+    } else {
+      if (path.extname(content.name) === '.VOB') {
+        positiveEvidence = true;
+      }
+    }
+  }
+  return positiveEvidence;
+}
+
+//Takes a full directory address and adds it to library
+function addDVDRip(folder) {
+  if isAlreadyInLibrary(folder) {
+    return;
+  } else {
+    addObj = {};
+    addObj.filename = folder;
+    addObj.title = path.basename(folder);
+    library.media.append(addObj);
+  }
+}
+
+//Takes a full file address and adds it to library
+function addVideoFile(file) {
+  if isAlreadyInLibrary(file) {
+    return;
+  } else {
+    addObj = {};
+    addObj.filename = file;
+    let fileExt = path.extname(file)
+    addObj.title = path.basename(file, fileExt);
+    library.media.append(addObj);
+  }
+}
+
+//Takes a full address for a file/folder and checks to see if
+//we already have it in the library.
+function isAlreadyInLibrary(address) {
+  let allMedia = library.media;
+  for (let i=0; i<media.length; i++) {
+    let currentMedia = media[i];
+    if (currentMedia.filename === address) {
+      return true;
+    }
+  }
+  return false;
 }
 
 ipcMain.on('settings-watchfolder-select', (event) => {
